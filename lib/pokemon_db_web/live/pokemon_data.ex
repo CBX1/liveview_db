054d04d7f088401dc31fc1f8390e46735cf9251e
RemSleep <keshav.1.rao@gmail.com>
2021-06-27 20:30:16 -0700

    Type Chart

--- lib/pokemon_db_web/live/pokemon_data.ex
+++ lib/pokemon_db_web/live/pokemon_data.ex
@@ -48,51 +48,51 @@ end
 def typeAdvantage(type) do
     cond do
         type == "NORMAL" ->
-            fillinBlanks(%{FIGHTING: 2, PSYCHIC: 2, GHOST: 0, FAIRY: 0.5})
+            fillinBlanks(%{FIGHTING: 2.0, PSYCHIC: 2.0, GHOST: 0, FAIRY: 0.5})
         type == "FIRE" ->
-            fillinBlanks(%{FIRE: 0.5, WATER: 2, GRASS: 0.5, ICE: 0.5, GROUND: 2, BUG: 0.5, ROCK: 2, STEEL: 0.5})
+            fillinBlanks(%{FIRE: 0.5, WATER: 2.0, GRASS: 0.5, ICE: 0.5, GROUND: 2.0, BUG: 0.5, ROCK: 2.0, STEEL: 0.5})
         type == "WATER" ->
-            fillinBlanks(%{FIRE: 0.5, WATER: 0.5, GRASS: 2, ELECTRIC: 2, ICE: 0.5, STEEL: 0.5})
+            fillinBlanks(%{FIRE: 0.5, WATER: 0.5, GRASS: 2.0, ELECTRIC: 2.0, ICE: 0.5, STEEL: 0.5})
         type == "ELECTRIC" ->
-            fillinBlanks(%{ELECTRIC: 0.5, GROUND: 2, FLYING: 0.5, STEEL: 0.5})
+            fillinBlanks(%{ELECTRIC: 0.5, GROUND: 2.0, FLYING: 0.5, STEEL: 0.5})
         type == "GRASS" ->
-            fillinBlanks(%{FIRE: 2, WATER: 0.5, ELECTRIC: 0.5, GRASS: 0.5, ICE: 2, GROUND: 0.5, FLYING: 2, BUG: 2})
+            fillinBlanks(%{FIRE: 2.0, WATER: 0.5, ELECTRIC: 0.5, GRASS: 0.5, ICE: 2.0, GROUND: 0.5, FLYING: 2.0, BUG: 2.0})
         type == "ICE" ->
-            fillinBlanks(%{FIRE: 2, WATER: 0.5, ICE: 0.5, FIGHTING: 2, ROCK: 2, STEEL: 2})
+            fillinBlanks(%{FIRE: 2.0, WATER: 0.5, ICE: 0.5, FIGHTING: 2.0, ROCK: 2.0, STEEL: 2.0})
         type == "FIGHTING" ->
-            fillinBlanks(%{FLYING: 2, PSYCHIC: 2, BUG: 0.5, ROCK: 0.5, DARK: 0.5, FAIRY: 2})
+            fillinBlanks(%{FLYING: 2.0, PSYCHIC: 2.0, BUG: 0.5, ROCK: 0.5, DARK: 0.5, FAIRY: 2.0})
         type == "POISON" ->
             fillinBlanks(%{POISON: 0.5, GRASS: 0.5, FIGHTING: 0.5, GROUND: 2, PSYCHIC: 2, BUG: 0.5, FAIRY: 0.5 })
         type == "GROUND" ->
-            fillinBlanks(%{WATER: 2, ELECTRIC: 0, GRASS: 2, ICE: 2, POISON: 0.5, ROCK: 0.5})
+            fillinBlanks(%{WATER: 2.0, ELECTRIC: 0, GRASS: 2.0, ICE: 2.0, POISON: 0.5, ROCK: 0.5})
         type == "FLYING" ->
-            fillinBlanks(%{ELECTRIC: 2, GRASS: 0.5, ICE: 2, FIGHTING: 0.5, GROUND: 0, BUG: 0.5, ROCK: 2})
+            fillinBlanks(%{ELECTRIC: 2.0, GRASS: 0.5, ICE: 2.0, FIGHTING: 0.5, GROUND: 0, BUG: 0.5, ROCK: 2.0})
         type == "PSYCHIC" ->
-            fillinBlanks(%{FIGHTING: 0.5, PSYCHIC: 0.5, BUG: 2, GHOST: 2, DARK: 2})
+            fillinBlanks(%{FIGHTING: 0.5, PSYCHIC: 0.5, BUG: 2.0, GHOST: 2.0, DARK: 2.0})
         type == "BUG" ->
-            fillinBlanks(%{FIRE: 2, GRASS: 0.5, FIGHTING: 0.5, GROUND: 0.5, FLYING: 2, ROCK: 2})
+            fillinBlanks(%{FIRE: 2.0, GRASS: 0.5, FIGHTING: 0.5, GROUND: 0.5, FLYING: 2.0, ROCK: 2.0})
         type == "STEEL" ->
-            fillinBlanks(%{NORMAL: 0.5, FIRE: 2, GRASS: 0.5, ICE: 0.5, FIGHTING: 2, POISON: 0, GROUND: 2, FLYING: 0.5, ROCK: 0.5, PSYCHIC: 0.5, BUG: 0.5, DRAGON: 0.5, STEEL: 0.5, FAIRY: 0.5})
+            fillinBlanks(%{NORMAL: 0.5, FIRE: 2.0, GRASS: 0.5, ICE: 0.5, FIGHTING: 2.0, POISON: 0, GROUND: 2.0, FLYING: 0.5, ROCK: 0.5, PSYCHIC: 0.5, BUG: 0.5, DRAGON: 0.5, STEEL: 0.5, FAIRY: 0.5})
         type == "FAIRY" ->
-            fillinBlanks(%{NORMAL: 2, FIGHTING: 0.5, POISON: 2, DRAGON: 0, DARK: 0.5, GHOST: 0.5, STEEL: 2})
+            fillinBlanks(%{NORMAL: 2.0, FIGHTING: 0.5, POISON: 2.0, DRAGON: 0, DARK: 0.5, GHOST: 0.5, STEEL: 2.0})
         type == "GHOST" ->
             fillinBlanks(%{NORMAL: 0, FIGHTING: 0, POISON: 0.5, BUG: 0.5, GHOST: 2, DARK: 2})
         type == "ROCK" ->
-          fillinBlanks(%{NORMAL: 0.5, WATER: 2, GRASS: 2, FIRE: 0.5, STEEL: 2, FIGHTING: 2, FLYING: 0.5, POISON: 0.5})
+          fillinBlanks(%{NORMAL: 0.5, WATER: 2.0, GRASS: 2.0, FIRE: 0.5, STEEL: 2.0, FIGHTING: 2.0, FLYING: 0.5, POISON: 0.5})
         type == "DARK" ->
-          fillinBlanks(%{BUG: 2, FIGHTING: 2, PSYCHIC: 0, FAIRY: 2, GHOST: 0.5, DARK: 0.5})
+          fillinBlanks(%{BUG: 2.0, FIGHTING: 2.0, PSYCHIC: 0, FAIRY: 2.0, GHOST: 0.5, DARK: 0.5})
         type == "DRAGON" ->
-          fillinBlanks(%{ICE: 2, GRASS: 0.5, WATER: 0.5, FIRE: 0.5, FAIRY: 2, DRAGON: 2})
+          fillinBlanks(%{ICE: 2.0, GRASS: 0.5, WATER: 0.5, FIRE: 0.5, FAIRY: 2.0, DRAGON: 2.0})
     end
 end
 
 def fillinBlanks(typearr) do
-    map = %{NORMAL: 1, FIRE: 1, WATER: 1,
-        ELECTRIC: 1, GRASS: 1, ICE: 1,
-        POISON: 1, GROUND: 1, FLYING: 1,
-        PSYCHIC: 1, BUG: 1, ROCK: 1,
-        GHOST: 1, DRAGON: 1, DARK: 1,
-        STEEL: 1, FAIRY: 1}
+    map = %{NORMAL: 1.0, FIRE: 1.0, WATER: 1.0,
+        ELECTRIC: 1.0, GRASS: 1.0, ICE: 1.0,
+        POISON: 1.0, GROUND: 1.0, FLYING: 1.0,
+        PSYCHIC: 1.0, BUG: 1.0, ROCK: 1.0,
+        GHOST: 1.0, DRAGON: 1.0, DARK: 1.0,
+        STEEL: 1.0, FAIRY: 1.0, FIGHTING: 1.0}
   Map.merge(typearr, map, fn _k, v1, v2 ->
     v1
   end )
