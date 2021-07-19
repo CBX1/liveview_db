385b93b2174f3f7d3eb1e11fcce2ca7a0ea162be
CBX1 <keshav.1.rao@gmail.com>
2021-07-18 21:44:26 -0700

    Only pkmn seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,9 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-PokemonDb.MoveData.read
-PokemonDb.Abil.read
-# PokemonDb.Main.read
+# PokemonDb.MoveData.read
+# PokemonDb.Abil.read
+PokemonDb.Main.read
 # PokemonDb.Location.read
 # PokemonDb.C.read
 # PokemonDb.Tno.read
