377b096e0939a1bbd755f3b0bbf1d17df0ccb5d8
CBX1 <keshav.1.rao@gmail.com>
2021-07-21 13:49:09 -0700

    custom domain

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pokemon_db, PokemonDbWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "desopbs.tk", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: [
+    "//desopbs.tk",
+    "//www.desopbs.tk",
+    "//twin-sour-bonobo.gigalixirapp.com//"
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
